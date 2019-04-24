a3017aedc42320b5670f8ea703c292a4c1fb0159
AMShonekan <a.shonekan96@gmail.com>
2019-04-24 21:59:19 +0100

    Setting up services

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import devBundle from './devBundle'
+import path from 'path'
+import template from './../template'
+import { MongoClient } from 'mongodb'
+
+const app = express()
+const CURRENT_WORKING_DIR = process.cwd()
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mern_project'
+
+let port = process.env.PORT || 3000
+
+devBundle.compile(app)
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err)
+    }
+    console.info(' Server started on port %s.', port)
+})
+MongoClient.connect(url, (err, db) => {
+    console.log("Connected successfully to mongodb server")
+    db.close()
+})
